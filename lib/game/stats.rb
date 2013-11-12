50b5147846db88249c037e6cadcd71395d378402
Graham Powrie <graham@asbp.io>
2013-11-12 10:15:57 -0500

    refactored Redis keys abit

--- lib/game/stats.rb
+++ lib/game/stats.rb
@@ -6,39 +6,45 @@ module Game
     def point_tally(user, skill, points, reverse=false)
       points = reverse ? (points * -1) : points
       REDIS.hincrby "user:#{user.id}:tallies", "points", points
+      
+      list_key = "user:#{user.id}:points:#{skill.id}"
       if reverse
-        REDIS.lpop "user:#{user.id}:points:#{skill.id}"
+        REDIS.lpop list_key
       else
-        REDIS.lpush "user:#{user.id}:points:#{skill.id}", points
-        REDIS.ltrim "user:#{user.id}:points:#{skill.id}", 0, LIST_STORAGE
+        REDIS.lpush list_key, points
+        REDIS.ltrim list_key, 0, LIST_STORAGE
       end
     end
   
     def skill_tally(user, skill, reverse=false)
-      REDIS.hincrby "user:#{user.id}:tallies", "skill:#{skill.id}", tally_inc(reverse)
-      REDIS.hincrby "user:#{user.id}:tallies", "category:#{skill.category}", tally_inc(reverse)
+      key = "user:#{user.id}:tallies"
+      REDIS.hincrby key, "skill:#{skill.id}", tally_inc(reverse)
+      REDIS.hincrby key, "category:#{skill.category}", tally_inc(reverse)
     end
   
     def condition_tally(user, skill, condition, reverse=false)
-      REDIS.hincrby "user:#{user.id}:tallies", "skill:#{skill.id}:condition:#{condition.id}", tally_inc(reverse)
-      REDIS.hincrby "user:#{user.id}:tallies", "condition:#{condition.id}", tally_inc(reverse)
+      key = "user:#{user.id}:tallies"
+      REDIS.hincrby key, "skill:#{skill.id}:condition:#{condition.id}", tally_inc(reverse)
+      REDIS.hincrby key, "condition:#{condition.id}", tally_inc(reverse)
     end
   
     def skill_performance(user, skill, points, reverse=false)
+      list_key = "user:#{user.id}:performance:#{skill.id}"
       if reverse
-        REDIS.lpop "user:#{user.id}:performance:#{skill.id}"
+        REDIS.lpop list_key
       else
-        REDIS.lpush "user:#{user.id}:performance:#{skill.id}", points
-        REDIS.ltrim "user:#{user.id}:performance:#{skill.id}", 0, LIST_STORAGE*10 # assume ten times as many skills entryies as sessions
+        REDIS.lpush list_key, points
+        REDIS.ltrim list_key, 0, LIST_STORAGE*10 # assume ten times as many skills entryies as sessions
       end
     end
     
     def difficulty_performance(user, skill, difficulty, reverse=false)
+      list_key = "user:#{user.id}:difficulty:#{skill.id}"
       if reverse
-        REDIS.lpop "user:#{user.id}:difficulty:#{skill.id}"
+        REDIS.lpop list_key
       else
-        REDIS.lpush "user:#{user.id}:difficulty:#{skill.id}", difficulty
-        REDIS.ltrim "user:#{user.id}:difficulty:#{skill.id}", 0, LIST_STORAGE
+        REDIS.lpush list_key, difficulty
+        REDIS.ltrim list_key, 0, LIST_STORAGE
       end
     end
   
